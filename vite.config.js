665c66488f1d2ad05454333f8a24a5631c3f0375
blinkzdlfx <blinkzdlfx@gmail.com>
2026-09-25 00:20:16 +0200

    Configure Vite for all multi-page HTML entries

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
+
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+
+    rollupOptions: {
+      input: {
+        index: resolve('.', 'index.html'),
+        about: resolve('.', 'about.html'),
+        academics: resolve('.', 'academics.html'),
+        admissions: resolve('.', 'admissions.html'),
+        schoolLife: resolve('.', 'school-life.html'),
+        news: resolve('.', 'news.html'),
+        gallery: resolve('.', 'gallery.html'),
+        contact: resolve('.', 'contact.html'),
+      },
+    },
   },
+
   server: {
     port: 3000,
     open: true,
